b198bb532c229871e5a6463345dd2bf563624be6
jeffersonarpasserini <jefferson.passerini@gmail.com>
2024-04-29 19:42:56 -0300

    pastas de projeto

---
+++ scholarstock/src/main/java/com/scholarstock/scholarstock/ScholarstockApplication.java
@@ -0,0 +1,13 @@
+package com.scholarstock.scholarstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarstockApplication.class, args);
+	}
+
+}
---
+++ scholarstock/src/test/java/com/scholarstock/scholarstock/ScholarstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholarstock.scholarstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
