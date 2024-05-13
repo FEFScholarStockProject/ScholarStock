08c8e4a2f60611f9a8b1833ef8eaa34ffde7685f
Milton-figueiredo <milton.v.figueiredo@gmail.com>
2024-05-13 20:22:05 -0300

    Enum TipoRequisicao

---
+++ scholarstock/src/main/java/com/scholarstock/domains/enums/TipoRequisicao.java
@@ -0,0 +1,40 @@
+package com.scholarstock.domains.enums;
+
+public enum TipoRequisicao {
+
+    RequisicaoEntrada(0,"RequisicaoEntrada"), RequisicaoSaida(1,"RequisicaoSaida");  
+
+    private Integer id;
+    private String tipoRequisicao;
+    
+    private TipoRequisicao(Integer id, String tipoRequisicao) {
+        this.id = id;
+        this.tipoRequisicao = tipoRequisicao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipoRequisicao() {
+        return tipoRequisicao;
+    }
+
+    public void setTipoRequisicao(String tipoRequisicao) {
+        this.tipoRequisicao = tipoRequisicao;
+    }
+
+    public static TipoRequisicao toEnum(Integer id){
+        if(id==null) return null;
+        for(TipoRequisicao x : TipoRequisicao.values()){
+            if(id.equals(x.getId())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Tipo da requisição não é valido.");
+    }
+}
