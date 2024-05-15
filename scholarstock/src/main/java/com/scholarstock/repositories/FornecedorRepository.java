bbd1ae6b3037affeb1d5f16cc62880f79e7b1145
Milton-figueiredo <milton.v.figueiredo@gmail.com>
2024-05-14 21:47:04 -0300

    fornecedor

--- scholarstock/src/main/java/com/scholarstock/repositories/FornecedorRepository.java
+++ scholarstock/src/main/java/com/scholarstock/repositories/FornecedorRepository.java
@@ -1,5 +1,15 @@
 package com.scholarstock.repositories;
 
-public interface FornecedorRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scholarstock.domains.Fornecedor;
+
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long>{
+    
+    List<Fornecedor> findByCnpj(String cnpj);
 }
