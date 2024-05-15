895d60d0900689e082da9e9d26a60ba476ca874a
Mateus Ferreira <matefe1200@gmail.com>
2024-05-14 21:57:42 -0300

    domain e o repository de UnidadeReceptora

--- scholarstock/src/main/java/com/scholarstock/domains/enums/TipoMovimento.java
+++ scholarstock/src/main/java/com/scholarstock/domains/enums/TipoMovimento.java
@@ -1,14 +1,15 @@
 package com.scholarstock.domains.enums;
 
 public enum TipoMovimento {
-    ENTRADA(0,"ENTRADA"), SAIDA(1,"SAIDA"), ACERTO_ENTRADA(2,"ACERTO_ENTRADA"), ACERTO_SAIDA(3,"ACERTO_SAIDA"), DOACAO(4,"DOACAO");
+    ENTRADA(0, "ENTRADA"), SAIDA(1, "SAIDA"), ACERTO_ENTRADA(2, "ACERTO_ENTRADA"), ACERTO_SAIDA(3, "ACERTO_SAIDA"),
+    DOACAO(4, "DOACAO");
 
     private Integer id;
-    private String TipoMovimento;
-    
+    private String tipoMovimento;
+
     private TipoMovimento(Integer id, String tipoMovimento) {
         this.id = id;
-        this.TipoMovimento = tipoMovimento;
+        this.tipoMovimento = tipoMovimento;
     }
 
     public Integer getId() {
@@ -20,17 +21,18 @@ public enum TipoMovimento {
     }
 
     public String getTipoMovimento() {
-        return TipoMovimento;
+        return tipoMovimento;
     }
 
     public void setTipoMovimento(String tipoMovimento) {
-        this.TipoMovimento = tipoMovimento;
+        this.tipoMovimento = tipoMovimento;
     }
-    
+
     public static TipoMovimento toEnum(Integer id) {
-        if (id == null) return null;
-        for(TipoMovimento x : TipoMovimento.values()){
-            if(id.equals(x.getId())){
+        if (id == null)
+            return null;
+        for (TipoMovimento x : TipoMovimento.values()) {
+            if (id.equals(x.getId())) {
                 return x;
             }
         }
