57ed5895d22f1d5b5ec9711c759396b27bada588
LuisFelipePdaSilva <l.f.prado.silva@gmail.com>
2024-05-14 21:53:57 -0300

    Adicionando Domain e Repository de Pessoa

---
+++ scholarstock/src/main/java/com/scholarstock/domains/Pessoa.java
@@ -0,0 +1,176 @@
+package com.scholarstock.domains;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.scholarstock.domains.enums.Situacao;
+import com.scholarstock.domains.enums.TipoPessoa;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String rg;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    @Column(unique = true)
+    private String telefone;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "perfis")
+    private Set<Integer> tipoPessoa = new HashSet<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "situacao")
+    private Set<Integer> situacao = new HashSet<>();
+
+    public Pessoa() {
+        super();
+        addTipoPessoa(TipoPessoa.SERVIDOR);
+        addSituacao(Situacao.INATIVO);
+    }
+
+    public Pessoa(Long id, String nome, String cpf, String rg, LocalDate dataNascimento, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        addTipoPessoa(TipoPessoa.SERVIDOR);
+        addSituacao(Situacao.INATIVO);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Set<TipoPessoa> getTipoPessoa() {
+        return tipoPessoa.stream().map(x -> TipoPessoa.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addTipoPessoa(TipoPessoa tipoPessoa) {
+        this.tipoPessoa.add(tipoPessoa.getId());
+    }
+
+    public Set<Situacao> getSituacao() {
+        return situacao.stream().map(x -> Situacao.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addSituacao(Situacao situacao) {
+        this.situacao.add(situacao.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (rg == null) {
+            if (other.rg != null)
+                return false;
+        } else if (!rg.equals(other.rg))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        return true;
+    }
+    
+}
