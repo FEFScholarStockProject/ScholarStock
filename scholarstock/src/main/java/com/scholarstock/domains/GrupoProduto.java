5322201b1a889454c56601f04ba1f9760f972585
jeffersonarpasserini <jefferson.passerini@gmail.com>
2024-05-16 21:40:15 -0300

    grupoproduto correcao importacao

--- scholarstock/src/main/java/com/scholarstock/domains/GrupoProduto.java
+++ scholarstock/src/main/java/com/scholarstock/domains/GrupoProduto.java
@@ -2,7 +2,6 @@ package com.scholarstock.domains;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.scholarstock.domains.enums.Situacao;
