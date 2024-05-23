09b1d7cf04fb152f8f545d5942cdfbe4b1453c3b
jeffersonarpasserini <jeffeson.passerini@gmail.com>
2024-05-23 10:25:14 -0300

    correção atributos classe fornecedor

--- scholarstock/src/main/java/com/scholarstock/domains/Fornecedor.java
+++ scholarstock/src/main/java/com/scholarstock/domains/Fornecedor.java
@@ -23,12 +23,15 @@ public class Fornecedor {
 
     @NotNull
     @Column (unique = true)
-    private String cnpj;
+    private String cpfCnpj;
 
     @Column
     private String razaoSocial;
     private String nomeFantasia;
-    private String endereco;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String complemento;
     private String cidade;
     private String estado;
     private String cep;
@@ -46,109 +49,165 @@ public class Fornecedor {
         setSituacao(Situacao.ATIVO);
     }
 
-    public Fornecedor(Long id, @NotNull String cnpj, String razaoSocial, String nomeFantasia, String endereco,
-            String cidade, String estado, String cep, int telefone) {
+
+    public Fornecedor(Long id, @NotNull String cpfCnpj, String razaoSocial, String nomeFantasia, String rua,
+            String numero, String bairro, String complemento, String cidade, String estado, String cep, int telefone,
+            Situacao situacao) {
         this.id = id;
-        this.cnpj = cnpj;
+        this.cpfCnpj = cpfCnpj;
         this.razaoSocial = razaoSocial;
         this.nomeFantasia = nomeFantasia;
-        this.endereco = endereco;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
         this.cidade = cidade;
         this.estado = estado;
         this.cep = cep;
         this.telefone = telefone;
-        setSituacao(Situacao.ATIVO);
+        this.situacao = situacao;
     }
+
 
     public Long getId() {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getCnpj() {
-        return cnpj;
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
     }
 
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
     }
+
 
     public String getRazaoSocial() {
         return razaoSocial;
     }
 
+
     public void setRazaoSocial(String razaoSocial) {
         this.razaoSocial = razaoSocial;
     }
+
 
     public String getNomeFantasia() {
         return nomeFantasia;
     }
 
+
     public void setNomeFantasia(String nomeFantasia) {
         this.nomeFantasia = nomeFantasia;
     }
 
-    public String getEndereco() {
-        return endereco;
+
+    public String getRua() {
+        return rua;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+
+    public void setRua(String rua) {
+        this.rua = rua;
     }
+
+
+    public String getNumero() {
+        return numero;
+    }
+
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+
+    public String getBairro() {
+        return bairro;
+    }
+
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
 
     public String getCidade() {
         return cidade;
     }
 
+
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
+
 
     public String getEstado() {
         return estado;
     }
 
+
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
 
     public String getCep() {
         return cep;
     }
 
+
     public void setCep(String cep) {
         this.cep = cep;
     }
+
 
     public int getTelefone() {
         return telefone;
     }
 
+
     public void setTelefone(int telefone) {
         this.telefone = telefone;
     }
+
 
     public Situacao getSituacao() {
         return situacao;
     }
 
+
     public void setSituacao(Situacao situacao) {
         this.situacao = situacao;
     }
+
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((cnpj == null) ? 0 : cnpj.hashCode());
-        result = prime * result + telefone;
+        result = prime * result + ((cpfCnpj == null) ? 0 : cpfCnpj.hashCode());
         return result;
     }
+
 
     @Override
     public boolean equals(Object obj) {
@@ -164,14 +223,13 @@ public class Fornecedor {
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (cnpj == null) {
-            if (other.cnpj != null)
+        if (cpfCnpj == null) {
+            if (other.cpfCnpj != null)
                 return false;
-        } else if (!cnpj.equals(other.cnpj))
-            return false;
-        if (telefone != other.telefone)
+        } else if (!cpfCnpj.equals(other.cpfCnpj))
             return false;
         return true;
     }
 
+    
 }
